91b7f390bebd076d58f14c4c42e553c479e566b2
LimeStreem <LimeStreem@gmail.com>
2016-09-13 14:39:13 +0900

    fixed material converter bug

--- src/Converters/MaterialConverter.ts
+++ src/Converters/MaterialConverter.ts
@@ -4,7 +4,7 @@ import MaterialComponent from "../Components/MaterialComponent";
 
 function MaterialConverter(this: Attribute, val: any): any {
   if (typeof val === "string") {
-    const regex = /\s*new\s*\(\s*([a-zA-Z\d]+)\s*\)/;
+    const regex = /\s*new\s*\(\s*([a-zA-Z\d\-]+)\s*\)/;
     let regexResult: RegExpExecArray;
     if (regexResult = regex.exec(val)) { // new material should be instanciated for this material
       this.component[this.declaration["componentBoundTo"]] = null;
