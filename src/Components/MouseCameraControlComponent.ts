be41290b425becc88860a539ff901eba21b97dcd
moajo <mimirosiasd@gmail.com>
2016-10-29 17:56:44 +0900

    fixed mouseCameraControll

--- src/Components/MouseCameraControlComponent.ts
+++ src/Components/MouseCameraControlComponent.ts
@@ -5,71 +5,57 @@ import Component from "grimoirejs/lib/Node/Component";
 import IAttributeDeclaration from "grimoirejs/lib/Node/IAttributeDeclaration";
 
 export default class MouseCameraControlComponent extends Component {
-  public static rotateCoefficient: number = 0.003;
-
-  public static moveCoefficient: number = 0.05;
-
   public static attributes: { [key: string]: IAttributeDeclaration } = {
-    // Specify the attributes user can intaract
-    rotateX: {
-      defaultValue: 1,
+    rotateSpeed: {
+      defaultValue: 0.01,
       converter: "Number"
     },
-    rotateY: {
-      defaultValue: 1,
-      converter: "Number"
-    },
-    moveZ: {
-      defaultValue: 1,
+    zoomSpeed: {
+      defaultValue: 0.05,
       converter: "Number"
     },
     moveSpeed: {
-      defaultValue: 1,
+      defaultValue: 1,//TODO:小数にしたときの挙動
       converter: "Number"
     }
-
   };
 
+  //propaty bound to
   private _transform: TransformComponent;
-
-  private _scriptTag: HTMLScriptElement;
-
-  private _lastScreenPos: { x: number, y: number } = { x: NaN, y: NaN };
-
-  private _rotateX: number;
-
-  private _rotateY: number;
-
-  private _moveZ: number;
-
-  private _origin: Vector3 = new Vector3(0, 0, 0);
-
+  private _rotateSpeed: number;
+  private _zoomSpeed: number;
   private _moveSpeed: number;
 
-  private _xsum: number = 0;
-
-  private _ysum: number = 0;
+  private _origin: Vector3 = new Vector3(0, 0, 0);
+  private _lastScreenPos: { x: number, y: number } = null;
 
   private _initialDirection: Vector3;
-
   private _initialRotation: Quaternion;
 
-  private _distance: number;
+  //for rotation axis.
+  private _initialRight: Vector3;
+  private _initialUp: Vector3;
+
+  private _xsum: number = 0;
+  private _ysum: number = 0;
 
   public $awake(): void {
-    this.getAttribute("rotateX").boundTo("_rotateX");
-    this.getAttribute("rotateY").boundTo("_rotateY");
-    this.getAttribute("moveZ").boundTo("_moveZ");
+    this.getAttribute("rotateSpeed").boundTo("_rotateSpeed");
+    this.getAttribute("zoomSpeed").boundTo("_zoomSpeed");
     this.getAttribute("moveSpeed").boundTo("_moveSpeed");
     this._transform = this.node.getComponent("Transform") as TransformComponent;
-    this._scriptTag = this.companion.get("canvasElement");
   }
 
   public $mount(): void {
-    this._scriptTag.addEventListener("mousemove", this._mouseMove.bind(this));
-    this._scriptTag.addEventListener("contextmenu", this._contextMenu.bind(this));
-    this._scriptTag.addEventListener("mousewheel", this._mouseWheel.bind(this));
-    this._distance = Math.sqrt(Vector3.dot(this._transform.localPosition.subtractWith(this._origin), this._transform.localPosition.subtractWith(this._origin)));
+    this._initialRight = Vector3.copy(this._transform.right);
+    this._initialUp = Vector3.copy(this._transform.up);
+    this._initialDirection = this._transform.localPosition.subtractWith(this._origin);
+    this._initialRotation = this._transform.localRotation;
+
+    let scriptTag = this.companion.get("canvasElement");
+    scriptTag.addEventListener("mousemove", this._mouseMove.bind(this));
+    scriptTag.addEventListener("contextmenu", this._contextMenu.bind(this));
+    scriptTag.addEventListener("mousewheel", this._mouseWheel.bind(this));
   }
 
   private _contextMenu(m: MouseEvent): void {
@@ -77,32 +63,41 @@ export default class MouseCameraControlComponent extends Component {
   }
 
   private _mouseMove(m: MouseEvent): void {
-    if (isNaN(this._lastScreenPos.x)) {
-      this._initialDirection = this._transform.localPosition.subtractWith(this._origin);
-      this._initialRotation = this._transform.localRotation;
+    if (this._lastScreenPos === null) {
       this._lastScreenPos = {
         x: m.screenX,
         y: m.screenY
       };
     }
+
     let updated = false;
-    const diffX = m.screenX - this._lastScreenPos.x, diffY = m.screenY - this._lastScreenPos.y;
+    const diffX = m.screenX - this._lastScreenPos.x;
+    const diffY = m.screenY - this._lastScreenPos.y;
+    let distance = this._transform.localPosition.subtractWith(this._origin).magnitude;
     if ((m.buttons & 1) > 0) { // When left button was pressed
       this._xsum += diffX;
       this._ysum += diffY;
+      this._ysum = Math.min(Math.PI * 50, this._ysum);
+      this._ysum = Math.max(-Math.PI * 50, this._ysum);
       updated = true;
     }
-    if ((m.buttons & 2) > 0) { // When right button was pressed
-      this._origin = this._origin.addWith(this._transform.right.multiplyWith(-diffX * 0.05 * this._moveSpeed)).addWith(this._transform.up.multiplyWith(diffY * 0.05 * this._moveSpeed));
+    if ((m.buttons & 2) > 0) { // When right button was pressed, move origin.
+      let moveX = -diffX * this._moveSpeed * 0.01;
+      let moveY = diffY * this._moveSpeed * 0.01;
+      this._origin = this._origin.addWith(this._transform.right.multiplyWith(moveX)).addWith(this._transform.up.multiplyWith(moveY));
+      distance = this._transform.localPosition.subtractWith(this._origin).magnitude;
       updated = true;
-      this._distance = Math.sqrt(Vector3.dot(this._transform.localPosition.subtractWith(this._origin), this._transform.localPosition.subtractWith(this._origin)));
     }
 
     if (updated) {
-      const rotation = Quaternion.euler(this._ysum * 0.01, this._xsum * 0.01, 0);
+      // rotate excution
+      let rotationVartical = Quaternion.angleAxis(-this._xsum * this._rotateSpeed, this._initialUp);
+      let rotationHorizontal = Quaternion.angleAxis(-this._ysum * this._rotateSpeed, this._initialRight);
+      let rotation = Quaternion.multiply(rotationVartical, rotationHorizontal);
+
       const rotationMat = Matrix.rotationQuaternion(rotation);
       const direction = Matrix.transformNormal(rotationMat, this._initialDirection);
-      this._transform.localPosition = this._origin.addWith(Vector3.normalize(direction).multiplyWith(this._distance));
+      this._transform.localPosition = this._origin.addWith(Vector3.normalize(direction).multiplyWith(distance));
       this._transform.localRotation = Quaternion.multiply(this._initialRotation, rotation);
     }
     this._lastScreenPos = {
@@ -112,8 +107,17 @@ export default class MouseCameraControlComponent extends Component {
   }
 
   private _mouseWheel(m: MouseWheelEvent): void {
-    this._distance -= m.deltaY * this._moveZ * MouseCameraControlComponent.moveCoefficient;
-    this._transform.localPosition = this._transform.localPosition.addWith(this._transform.forward.multiplyWith(m.deltaY * this._moveZ * MouseCameraControlComponent.moveCoefficient));
+    // let move = m.deltaY * this._moveZ * MouseCameraControlComponent.moveCoefficient;
+    // let toOrigin = Vector3.normalize(Vector3.subtract(this._origin, this._transform.localPosition));
+    // this._origin = this._origin.addWith(toOrigin.multiplyWith(move));
+    // this._transform.localPosition = this._transform.localPosition.addWith(this._transform.forward.multiplyWith(move));
+    // m.preventDefault();
+
+    let dir = Vector3.normalize(Vector3.subtract(this._transform.localPosition, this._origin));
+    let moveDist = -m.deltaY * this._zoomSpeed;
+    let distance = Vector3.subtract(this._origin, this._transform.localPosition).magnitude;
+    let nextDist = Math.max(1, distance - moveDist);
+    this._transform.localPosition = this._origin.addWith(dir.multiplyWith(nextDist))
     m.preventDefault();
   }
 }
