c7d357676cbd86a55d036aaba027b6ce7044c891
moajo <mimirosiasd@gmail.com>
2016-12-03 16:53:49 +0900

    fix RotationParser

--- src/Util/RotationParser.ts
+++ src/Util/RotationParser.ts
@@ -12,11 +12,7 @@ export default class RotationParser {
    * @returns {number} parsed angle in radians.
    */
   public static parseAngle(input: string): number {
-    let num = Number.parseFloat(input);
-    if (!isNaN(num)) {
-      return num;
-    }
-    const regex = /^ *(-? *(?:0|[1-9]\d*)(?: *\.\d+)?) *(?:\/ *((?:0|[1-9]\d*)(?: *\.\d+)?))? *(p|prad|deg|d|r|rad)? *$/gm;
+    const regex = /^ *(-?[\de+-.]*) *(?:\/ *([\de+-.]*))? *(p|prad|deg|d|r|rad)? *$/gm;
     const result = regex.exec(input);
 
     if (result == null) {
