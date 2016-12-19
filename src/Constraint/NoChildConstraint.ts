6f0b2f26c43214d1a3ee813a6986a1eb604855d3
LimeStreem <LimeStreem@gmail.com>
2016-12-19 19:18:53 +0900

    Add fullscreenTarget attribute on FullScreenComponent Set style.height=100% on html and body tag when the parent of goml is body and height is 0

--- src/Constraint/NoChildConstraint.ts
+++
@@ -1,9 +0,0 @@
-import gr from "grimoirejs";
-import GomlNode from "grimoirejs/ref/Node/GomlNode";
-
-function NoChildConstraint(): ((self: GomlNode) => string) {
-  return (self: GomlNode) => {
-    return self.children.length === 0 ? null : "NoChildConstraint fail:" + self.name;
-  };
-}
-export default NoChildConstraint;
