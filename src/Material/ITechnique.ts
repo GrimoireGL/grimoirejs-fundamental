7ddbb91f09fec87897d98d580cf3c749f660a3a7
LimeStreem <LimeStreem@gmail.com>
2016-12-20 10:41:43 +0900

    fixed material converter problem

---
+++ src/Material/ITechnique.ts
@@ -0,0 +1,19 @@
+interface ITechniqueRecipe {
+  fragment: string;
+  vertex: string;
+  uniforms: {
+    [key: string]: {
+      type: number;
+      count: number;
+      default: any;
+      semantic: string;
+      node?: string;
+    };
+  },
+  attributes: {
+    [key: string]: string; // variable-name to semantic key
+  },
+  states: {
+    enable: number[]
+  }
+}
