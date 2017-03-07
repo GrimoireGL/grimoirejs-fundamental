2d9adc07264e3a142289371b3c080980c1124b36
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-03-07 10:35:12 +0900

    feat: add new field 'renderArgs' for passing arguments to uniform register for each MeshRenderer to IRenderable

--- src/SceneRenderer/IRenderable.ts
+++ src/SceneRenderer/IRenderable.ts
@@ -1,9 +1,10 @@
 import CameraComponent from "../Components/CameraComponent";
 import IRenderArgument from "./IRenderArgument";
 interface IRenderable {
-  index:number;
-  getRenderingPriorty(camera: CameraComponent, cameraUpdated: boolean, lastPriorty: number): number;
-  render(renderArg: IRenderArgument): void;
-  setRenderableIndex(index:number);
+    index: number;
+    renderArgs: { [key: string]: any };
+    getRenderingPriorty(camera: CameraComponent, cameraUpdated: boolean, lastPriorty: number): number;
+    render(renderArg: IRenderArgument): void;
+    setRenderableIndex(index: number);
 }
 export default IRenderable;
