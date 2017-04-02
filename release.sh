f2cbcb3e10d976ddc519e02d681b0e19e054d098
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-04-02 12:24:09 +0900

    fix: syntax error of release code

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 npm run semantic-release
-if ["$?" = "0"]; then
+if [ "$?" = "0" ]; then
   curl https://preset-updater.herokuapp.com/?repo=preset-basic&repoURL=https://github.com/GrimoireGL/grimoirejs-preset-basic.git&buildNumber=$CIRCLE_BUILD_NUM&currentPkg=grimoirejs-fundamental
 fi
 exit 0
