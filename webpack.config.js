08521cdf83adb0475ee67f7a2102a96349b068ca
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-06-04 14:24:00 +0900

    chore(build-script):Fixed watch script

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const fs = require("fs");
+
 // Shot package name will be used for prefix of filename
 const fnPrefix = JSON.parse(fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")).name.replace("grimoirejs", "grimoire");
 
@@ -14,6 +15,28 @@ class CopyPlugin {
     compiler.plugin("after-emit", (compiler, callback) => {
       fs.createReadStream(this.copyFrom).pipe(fs.createWriteStream(this.copyTo));
       callback();
+    });
+  }
+}
+
+// Couldron generate-expose and generate-reference execute with build tasks.
+class CauldronPlugin {
+  apply(compiler) {
+    compiler.plugin("compile", () => {
+      try {
+        console.log(require('child_process').execSync('npm run generate-expose').toString());
+      } catch (e) {
+        console.log(e);
+      }
+    });
+    compiler.plugin('after-emit', (compiler, callback) => {
+      try {
+        console.log(require('child_process').execSync('npm run generate-reference').toString());
+      } catch (e) {
+        console.log(e);
+      } finally {
+        callback();
+      }
     });
   }
 }
@@ -51,26 +74,24 @@ module.exports = (env) => {
   let buildTasks = [];
   let isDefault = !env.browser && !env.npm && !env.prod;
   let skipIndex = false;
-  console.log(env.prod);
+  let cauldron = new CauldronPlugin();
   if (env.browser || env.prod) {
-    const plugins = [];
+    const plugins = [cauldron];
     // if needs index also and it was not including core, index.js must be copied from fnPrefix.js
     // since these are completely same build task. Yeah, optimization.
     if (!includeCore && (env.prod || env.npm)) {
       plugins.push(new CopyPlugin(`./register/${fnPrefix}.js`, './register/index.js'));
       plugins.push(new CopyPlugin(`./register/${fnPrefix}.js.map`, './register/index.js.map'));
       skipIndex = true;
-      console.log('index.js would be copied from original.');
     }
     buildTasks.push(getBuildTask(`${fnPrefix}.js`, plugins, polyfills[0]));
   }
   if (!skipIndex && (isDefault || env.npm || env.prod)) {
-    console.log('index.js would be generated separately');
-    buildTasks.push(getBuildTask("index.js", [], polyfills[1]));
+    buildTasks.push(getBuildTask("index.js", [cauldron], polyfills[1]));
   }
   if (env.prod) {
-    console.log('Minified version would be generated also');
     buildTasks.push(getBuildTask(fnPrefix + ".min.js", [
+      cauldron,
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
