b4ee994a4d377f7356e9f2da029e73d70283c04c
case2912 <shiroyama@maia.eonet.ne.jp>
2017-07-27 17:35:37 +0900

    fix: delete src/Material/DefaultMaterial.ts

--- src/Material/DefaultMaterial.ts
+++
@@ -1,8 +0,0 @@
-import MaterialFactory from "./MaterialFactory";
-import Unlit from "raw-loader!./Static/Unlit.sort";
-
-export default class DefaultMaterial {
-  public static register(): void {
-    MaterialFactory.addSORTMaterial("unlit", Unlit);
-  }
-}
