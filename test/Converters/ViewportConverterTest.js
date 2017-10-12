10ec3aa58d0c72e1f3e0af009b8eb5073168093a
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-10-12 14:01:35 +0900

    fix: rewrite test in typescript

--- test/Converters/ViewportConverterTest.js
+++
@@ -1,5 +0,0 @@
-import "../TestInit";
-import test from 'ava';
-import ViewportConverter from '../../lib-es5/Converters/ViewportConverter';
-
-test.todo("Write ViewportConverter");
---
+++ test/Converters/ViewportConverterTest.ts
@@ -0,0 +1,6 @@
+import "../TestInit";
+// tslint:disable-next-line:ordered-imports
+import test from "ava";
+import ViewportConverter from "../../src/Converters/ViewportConverter";
+
+test.todo("Write ViewportConverter");
