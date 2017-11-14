01dc99889089fd7990ce36388ac9ad7851bf678b
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-11-14 18:02:09 +0900

    feat: refactor texture API

--- src/Resource/Texture.ts
+++ src/Resource/Texture.ts
@@ -6,6 +6,8 @@ export default abstract class Texture extends GLResource<WebGLTexture> {
 
     private static _resizerCanvas: HTMLCanvasElement = document.createElement("canvas");
 
+    private static _resizerContext = Texture._resizerCanvas.getContext("2d");
+
     /**
     * ミップマップの更新が必要なフィルタ
     * @type {number[]}
@@ -17,7 +19,74 @@ export default abstract class Texture extends GLResource<WebGLTexture> {
         WebGLRenderingContext.NEAREST_MIPMAP_NEAREST,
     ];
 
-    constructor(gl: WebGLRenderingContext) {
+    public get magFilter(): number {
+        return this._magFilter;
+    }
+
+    public set magFilter(filter: number) {
+        if (this._magFilter !== filter) {
+            this._texParameterChanged = true;
+            this._magFilter = filter;
+        }
+    }
+
+    public get minFilter(): number {
+        return this._minFilter;
+    }
+
+    public set minFilter(filter: number) {
+        if (this._minFilter !== filter) {
+            this._texParameterChanged = true;
+            this._minFilter = filter;
+            this.__ensureMipmap();
+        }
+    }
+
+    public get wrapS(): number {
+        return this._wrapS;
+    }
+
+    public set wrapS(filter: number) {
+        if (this._wrapS !== filter) {
+            this._texParameterChanged = true;
+            this._wrapS = filter;
+        }
+    }
+
+    public get wrapT(): number {
+        return this._wrapT;
+    }
+
+    public set wrapT(filter: number) {
+        if (this._wrapT !== filter) {
+            this._texParameterChanged = true;
+            this._wrapT = filter;
+        }
+    }
+
+    public get format(): number {
+        return this.__format;
+    }
+
+    public get type(): number {
+        return this.__type;
+    }
+
+    protected __format: number = WebGLRenderingContext.RGBA;
+
+    protected __type: number = WebGLRenderingContext.UNSIGNED_BYTE;
+
+    private _texParameterChanged = true;
+
+    private _magFilter: number = WebGLRenderingContext.LINEAR;
+
+    private _minFilter: number = WebGLRenderingContext.LINEAR;
+
+    private _wrapS: number = WebGLRenderingContext.REPEAT;
+
+    private _wrapT: number = WebGLRenderingContext.REPEAT;
+
+    constructor(gl: WebGLRenderingContext, public textureType: number) {
         super(gl, gl.createTexture());
     }
 
@@ -26,12 +95,18 @@ export default abstract class Texture extends GLResource<WebGLTexture> {
         this.gl.deleteTexture(this.resourceReference);
     }
 
-    protected __getRawPixels<T extends ArrayBufferView = ArrayBufferView>(type: number, format: number, x = 0, y = 0, width: number, height: number): T {
+    public register(registerNumber: number): void {
+        this.gl.activeTexture(WebGLRenderingContext.TEXTURE0 + registerNumber);
+        this.gl.bindTexture(this.textureType, this.resourceReference);
+        this.__applyTexParameter();
+    }
+
+    protected __getRawPixels<T extends ArrayBufferView = ArrayBufferView>(type: number, format: number, x = 0, y = 0, width: number, height: number, from: number): T {
         const bufferCtor = GLUtility.typeToTypedArrayConstructor(type);
         const buffer = new bufferCtor(width * height * GLUtility.formatToElementCount(format));
         const frame = this.gl.createFramebuffer();
         this.gl.bindFramebuffer(WebGLRenderingContext.FRAMEBUFFER, frame);
-        this.gl.framebufferTexture2D(WebGLRenderingContext.FRAMEBUFFER, WebGLRenderingContext.COLOR_ATTACHMENT0, WebGLRenderingContext.TEXTURE_2D, this.resourceReference, 0);
+        this.gl.framebufferTexture2D(WebGLRenderingContext.FRAMEBUFFER, WebGLRenderingContext.COLOR_ATTACHMENT0, from, this.resourceReference, 0);
         if (this.gl.checkFramebufferStatus(WebGLRenderingContext.FRAMEBUFFER) === WebGLRenderingContext.FRAMEBUFFER_COMPLETE) {
             this.gl.readPixels(x, y, width, height, format, type, buffer);
         }
@@ -39,31 +114,56 @@ export default abstract class Texture extends GLResource<WebGLTexture> {
         return buffer as T;
     }
 
-    protected __justifyResource(image: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement): IResizeResult {
+    /**
+     * Ensure specified resource is POT(Power of Two) resource.
+     * If speciefied resource was NPOT, resize specified resource to POT.
+     * @param image
+     */
+    protected __ensurePOT(image: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement): IResizeResult {
         if (image instanceof HTMLImageElement) {
-            return this.__justifyImage(image);
+            return this._ensureImagePOT(image);
         } else if (image instanceof HTMLCanvasElement) {
-            return this.__justifyCanvas(image);
+            return this._ensureCanvasPOT(image);
         } else if (image instanceof HTMLVideoElement) {
-            return this.__justifyVideo(image);
+            return this._ensureVideoPOT(image);
         } else {
             throw new Error("Unsupported resource type");
         }
     }
 
+    protected abstract __ensureMipmap(): void;
+
+    /**
+     * Check specified min filter requires mip map or not.
+     * @param minFilter min filter type
+     */
+    protected __needMipmap(minFilter: number): boolean {
+        return Texture._filtersNeedsMipmap.indexOf(minFilter) > -1;
+    }
+
+    /**
+     * Apply texParameteri parameters before updating texture
+     */
+    protected __applyTexParameter(): void {
+        if (!this._texParameterChanged) {
+            return;
+        }
+        this.gl.texParameteri(this.textureType, WebGLRenderingContext.TEXTURE_MIN_FILTER, this._minFilter);
+        this.gl.texParameteri(this.textureType, WebGLRenderingContext.TEXTURE_MAG_FILTER, this._magFilter);
+        this.gl.texParameteri(this.textureType, WebGLRenderingContext.TEXTURE_WRAP_S, this._wrapS);
+        this.gl.texParameteri(this.textureType, WebGLRenderingContext.TEXTURE_WRAP_T, this._wrapT);
+        this._texParameterChanged = false;
+    }
+
     // There should be more effective way to resize texture
-    protected __justifyImage(img: HTMLImageElement): IResizeResult {
+    private _ensureImagePOT(img: HTMLImageElement): IResizeResult {
         const w = img.naturalWidth, h = img.naturalHeight;
         const size = TextureSizeCalculator.getPow2Size(w, h);
         if (w !== size.width || h !== size.height) {
-            const canv = Texture._resizerCanvas;
-            canv.height = size.height;
-            canv.width = size.width;
-            canv.getContext("2d").drawImage(img, 0, 0, w, h, 0, 0, size.width, size.height);
             return {
-                result: canv,
-                height: canv.height,
-                width: canv.width,
+                result: this._resizeImageOrVideo(img, size.width, size.height),
+                height: size.height,
+                width: size.width,
             };
         }
         return {
@@ -73,7 +173,9 @@ export default abstract class Texture extends GLResource<WebGLTexture> {
         };
     }
 
-    protected __justifyCanvas(canvas: HTMLCanvasElement): IResizeResult {
+
+
+    private _ensureCanvasPOT(canvas: HTMLCanvasElement): IResizeResult {
         const w = canvas.width;
         const h = canvas.height;
         const size = TextureSizeCalculator.getPow2Size(w, h);
@@ -93,18 +195,14 @@ export default abstract class Texture extends GLResource<WebGLTexture> {
         };
     }
 
-    protected __justifyVideo(video: HTMLVideoElement): IResizeResult {
+    private _ensureVideoPOT(video: HTMLVideoElement): IResizeResult {
         const w = video.videoWidth, h = video.videoHeight;
         const size = TextureSizeCalculator.getPow2Size(w, h); // largest 2^n integer that does not exceed s
         if (w !== size.width || h !== size.height) {
-            const canv = Texture._resizerCanvas;
-            canv.height = size.height;
-            canv.width = size.width;
-            canv.getContext("2d").drawImage(video, 0, 0, w, h, 0, 0, size.width, size.height);
             return {
-                result: canv,
-                width: w,
-                height: h,
+                result: this._resizeImageOrVideo(video, size.width, size.height),
+                width: size.width,
+                height: size.height,
             };
         }
         return {
@@ -114,11 +212,11 @@ export default abstract class Texture extends GLResource<WebGLTexture> {
         };
     }
 
-    /**
-     * Check specified min filter requires mip map or not.
-     * @param minFilter min filter type
-     */
-    protected __needMipmap(minFilter: number): boolean {
-        return Texture._filtersNeedsMipmap.indexOf(minFilter) > -1;
+    private _resizeImageOrVideo(resource: HTMLImageElement | HTMLVideoElement, width: number, height: number): HTMLCanvasElement {
+        const canv = Texture._resizerCanvas;
+        canv.height = height;
+        canv.width = width;
+        Texture._resizerContext.drawImage(resource, 0, 0, resource.width, resource.height, 0, 0, width, height);
+        return Texture._resizerCanvas;
     }
 }
