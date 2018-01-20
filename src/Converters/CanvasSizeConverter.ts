80bc3c9cc0feb6c58f998b873d5485309755e84c
moajo <mimirosiasd@gmail.com>
2018-01-20 19:55:59 +0900

    fix: canvasInitializer

--- src/Converters/CanvasSizeConverter.ts
+++ src/Converters/CanvasSizeConverter.ts
@@ -1,29 +1,38 @@
 import CanvasSizeObject from "../Objects/CanvasSizeObject";
-/**
- * キャンバスのサイズ用のコンバーター
- * 数値を指定した場合(`100`など)はその値をそのまま返す。
- * `aspect(1.6)`などと記述する時、キャンバスのアスペクト比を1.6にするようにリサイズする。
- * `fit`と指定した時、親要素にちょうどマッチするサイズを返す。
- * もし、親要素の高さが0である時かつ、親がbodyである際で`fit`が指定されていると、bodyへの高さ属性が指定されていないものと判断して、
- * 自動的にbodyに`height:100%`を割り当てる。
- */
-export default function CanvasSizeConverter(val: any): any {
-  if (val === "fit") {
-    return {
-      mode: "fit",
-    } as CanvasSizeObject;
-  }
-  if (typeof val === "string") {
-    const matched = /aspect\(([\d+(?.\d*)?]+)\)/.exec(val);
-    if (matched) {
+
+
+export const CanvasSizeConverter = {
+  name: "CanvasSize",
+  /**
+   * キャンバスのサイズ用のコンバーター
+   * 数値を指定した場合(`100`など)はその値をそのまま返す。
+   * `aspect(1.6)`などと記述する時、キャンバスのアスペクト比を1.6にするようにリサイズする。
+   * `fit`と指定した時、親要素にちょうどマッチするサイズを返す。
+   * もし、親要素の高さが0である時かつ、親がbodyである際で`fit`が指定されていると、bodyへの高さ属性が指定されていないものと判断して、
+   * 自動的にbodyに`height:100%`を割り当てる。
+   */
+  convert(val: any): CanvasSizeObject | undefined {
+    if (val === "fit") {
       return {
-        mode: "aspect",
-        aspect: Number.parseFloat(matched[1]),
+        mode: "fit",
       };
     }
-  }
-  return {
-    mode: "manual",
-    size: Number.parseFloat(val),
-  } as CanvasSizeObject;
-}
+    if (typeof val === "string") {
+      const matched = /aspect\(([\d+(?.\d*)?]+)\)/.exec(val);
+      if (matched) {
+        return {
+          mode: "aspect",
+          aspect: Number.parseFloat(matched[1]),
+        };
+      }
+    }
+    const size = Number.parseFloat(val);
+    if (Number.isNaN(size)) {
+      return undefined;
+    }
+    return {
+      mode: "manual",
+      size: size,
+    };
+  },
+};
--- src/Objects/CanvasSizeObject.ts
+++ src/Objects/CanvasSizeObject.ts
@@ -1,6 +1,16 @@
-interface CanvasSizeObject {
-  mode: string;
-  size?: number;
-  aspect?: number;
+export type ResizeMode = CanvasSizeObject["mode"];
+export type CanvasSizeObject = CanvasAspectObject | CanvasManualObject | CanvasFitObject;
+
+export type CanvasAspectObject = {
+  mode: "aspect",
+  aspect: number,
 }
+export type CanvasManualObject = {
+  mode: "manual",
+  size: number,
+}
+export type CanvasFitObject = {
+  mode: "fit",
+}
+
 export default CanvasSizeObject;
